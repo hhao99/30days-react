0faf2210e59ab9919c4bf84edb66fe7eec116a3c
hhao99 <hhao99@163.com>
2018-01-31 18:04:24 +0800

    day5 t-1

--- src/day5/index.js
+++ src/day5/index.js
@@ -2,24 +2,28 @@ import React, { Component } from 'react'
 
 const apiKey = '4a172ab437d9871cb95cd47d26676729'
 const url='api.openweathermap.org/data/2.5/weather?q=London&appid='
+const url2='http://api.com/products.json'
 class Day5 extends Component {
   _getWeather() {
-    console.log(url+apiKey)
-    fetch(url+apiKey)
-      .then( response => {
-        console.log(response)
-        response.json() }
-      )
-      .then( json => {
-        console.log(json)
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    fetch(url2,{
+      json: true,
+      Access-Control-Allow-Origin: true,
+    })
+    .then( response => {
+      return response.json()
+    })
+    .then( json=> {
+      console.log(json)
+    })
+    .catch(err=> console.log(err))
+
 
   }
+  componentDidMount() {
+      this._getWeather()
+  }
   render() {
-    this._getWeather()
+
     return (
       <div>
         <h1>Day5 Weather App </h1>
